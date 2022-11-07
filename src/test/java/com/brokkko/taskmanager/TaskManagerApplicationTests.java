cc679edc6742fa396496cdfcf9bf430be6b88ae7
brokkko <nsstrannikova@mail.ru>
2022-11-08 00:18:05 +0300

    added services tests

--- src/test/java/com/brokkko/taskmanager/TaskManagerApplicationTests.java
+++ src/test/java/com/brokkko/taskmanager/TaskManagerApplicationTests.java
@@ -1,10 +1,23 @@
 package com.brokkko.taskmanager;
 
+import com.brokkko.taskmanager.initializer.PostgreSQL;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@ContextConfiguration(initializers = {
+        PostgreSQL.Initializer.class
+})
+@Testcontainers
 class TaskManagerApplicationTests {
+
+    @BeforeAll
+    public static void init() {
+        PostgreSQL.container.start();
+    }
 
     @Test
     void contextLoads() {
---
+++ src/test/java/com/brokkko/taskmanager/initializer/PostgreSQL.java
@@ -0,0 +1,25 @@
+package com.brokkko.taskmanager.initializer;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import javax.validation.constraints.NotNull;
+
+@UtilityClass
+public class PostgreSQL {
+    public static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:9.6.8");
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + container.getJdbcUrl(),
+                    "spring.datasource.username=" + container.getUsername(),
+                    "spring.datasource.password=" + container.getPassword()
+            ).applyTo(applicationContext);
+        }
+    }
+}
