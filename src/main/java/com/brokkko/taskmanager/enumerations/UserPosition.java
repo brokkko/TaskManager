20437fa81bd1ffbba132dd47fc113f46762ab109
brokkko <nsstrannikova@mail.ru>
2023-01-26 18:59:51 +0300

    added user position enum

---
+++ src/main/java/com/brokkko/taskmanager/enumerations/UserPosition.java
@@ -0,0 +1,10 @@
+package com.brokkko.taskmanager.enumerations;
+
+public enum UserPosition {
+    ANALYST_POSITION,
+    DEVELOPER_POSITION,
+    DESIGNER_POSITION,
+    TESTER_POSITION,
+    MANAGER_POSITION,
+    ARCHITECT_POSITION
+}
