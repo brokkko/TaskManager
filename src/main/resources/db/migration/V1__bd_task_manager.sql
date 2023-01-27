6295fa1f11a4e6611e337fa1fda85f87bd150cff
brokkko <nsstrannikova@mail.ru>
2023-01-27 14:46:46 +0300

    fixed migration sql

--- src/main/resources/db/migration/V1__bd_task_manager.sql
+++ src/main/resources/db/migration/V1__bd_task_manager.sql
@@ -1,41 +1,55 @@
 drop table if exists projects cascade;
 drop table if exists tasks cascade;
 drop table if exists team_projects cascade;
+drop table if exists user_team_projects cascade;
 drop table if exists users cascade;
 
 create table projects (
     id uuid not null,
     date_of_creation timestamp,
     description varchar(255),
-    name varchar(255),
+    name varchar(60),
     user_id uuid not null,
     primary key (id)
 );
+
 create table tasks (
     id uuid not null,
     date_of_creation timestamp,
     deadline timestamp,
     description varchar(255),
-    name varchar(255),
-    status varchar(255),
+    name varchar(100),
+    status varchar(20),
     time_estimate time,
     time_spent time,
     project_id uuid not null,
     user_id uuid not null,
     primary key (id)
 );
+
 create table team_projects (
-    project_id uuid not null,
-    primary key (project_id)
+   team_project_id uuid not null,
+   description varchar(255),
+   name varchar(30),
+   project_id uuid not null,
+   primary key (team_project_id)
 );
+
+create table user_team_projects (
+    position varchar(255),
+    user_id uuid not null,
+    team_project_team_project_id uuid,
+    primary key (user_id)
+);
+
 create table users (
     id uuid not null,
-    email varchar(255),
-    firstname varchar(255),
-    lastname varchar(255),
-    password varchar(255),
-    role varchar(255),
-    user_app_name varchar(255),
+    email varchar(50),
+    firstname varchar(50),
+    lastname varchar(50),
+    password varchar(100),
+    role varchar(20),
+    user_app_name varchar(53),
     primary key (id)
 );
 
@@ -51,10 +65,14 @@ alter table if exists tasks
     add constraint tasks_users_fk
     foreign key (user_id) references users on delete cascade;
 
--- alter table if exists team_projects_users
---     add constraint FK3c2uujcb7vhsdurmwc0av186w
---     foreign key (users_id) references users;
---
--- alter table if exists team_projects_users
---     add constraint FK4829a8bb3f2ad7acn51hwd3oa
---     foreign key (team_projects_project_id) references team_projects;
+alter table if exists team_projects
+    add constraint team_projects_projects_fk
+    foreign key (project_id) references projects on delete cascade;
+
+alter table if exists user_team_projects
+    add constraint user_team_projects_team_projects_fk
+    foreign key (team_project_team_project_id) references team_projects;
+
+alter table if exists user_team_projects
+    add constraint user_team_projects_users_fk
+    foreign key (user_id) references users;
